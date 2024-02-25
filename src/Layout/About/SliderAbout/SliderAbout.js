78fd0ae31bc66a97a076919a5cc015df243a7aa2
ali azizi <derehbeg@gmail.com>
2024-02-25 10:17:25 +0330

    add image new

--- src/Layout/About/SliderAbout/SliderAbout.js
+++ src/Layout/About/SliderAbout/SliderAbout.js
@@ -5,23 +5,23 @@ import "swiper/css/pagination";
 import "./SliderAbout.css";
 import { Pagination } from "swiper/modules";
 
-import Slide0 from "../../../image/photo slider/slider1.jpg";
-import Slide1 from "../../../image/photo slider/slider2.jpg";
-import Slide2 from "../../../image/photo slider/slider3.jpg";
-import Slide3 from "../../../image/photo slider/slider4.jpg";
-import Slide4 from "../../../image/photo slider/slider5.jpg";
-import Slide5 from "../../../image/photo slider/slider6.jpg";
-import Slide6 from "../../../image/photo slider/slider7.jpg";
+// import Slide0 from "../../../image/photo slider/slider1.jpg";
+import Slide1 from "../../../image/photo slider/slider3.jpg";
+import Slide2 from "../../../image/photo slider/slider2.jpg";
+import Slide3 from "../../../image/photo slider/slider14.jpg";
+// import Slide4 from "../../../image/photo slider/slider5.jpg";
+// import Slide5 from "../../../image/photo slider/slider6.jpg";
+// import Slide6 from "../../../image/photo slider/slider7.jpg";
 
 const SliderAbout = () => {
   const Slides = [
-    { id: 1, src: Slide0, title: "انواع سیمان" },
+    // { id: 1, src: Slide0, title: "انواع سیمان" },
     { id: 2, src: Slide1, title: "هبلکس(بلوک aac)" },
     { id: 3, src: Slide2, title: "انواع گچ سفید کاری" },
     { id: 4, src: Slide3, title: "گچ و خاک آماده" },
-    { id: 5, src: Slide4, title: "انواع سفال" },
-    { id: 6, src: Slide5, title: "انواع آجر طاقی" },
-    { id: 7, src: Slide6, title: "بلوک سیمانی" },
+    // { id: 5, src: Slide4, title: "انواع سفال" },
+    // { id: 6, src: Slide5, title: "انواع آجر طاقی" },
+    // { id: 7, src: Slide6, title: "بلوک سیمانی" },
   ];
 
   return (
